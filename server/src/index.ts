e1996fd0a10cb0253bfe18b55a194222b88061cd
muriz-dev <mhmdrizki.dev@gmail.com>
2026-08-04 22:24:08 +0700

    refactor: format, lint, and organize using biome check

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,17 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+export const app = new Elysia().get("/", () => "Hello Elysia");
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.get("/books", () => {
+	return {
+		data: [],
+	};
+});
+
+if (import.meta.main) {
+	app.listen(3000);
+
+	console.log(
+		`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+	);
+}
