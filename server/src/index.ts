805c6a77d3fd16e910b40ece3a9351c82bdd39ac
Your Name <you@example.com>
2026-08-13 21:11:05 +0700

    build(server): register app logger, security header and error hanlder

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from "elysia";
+import env from "./config/env";
+import { appLogger } from "./utils/logger";
+import { securityHeaders } from "./middlewares/security-header";
+import { errorHandler } from "./middlewares/error-handler";
 
-export const app = new Elysia().get("/", () => "Hello Elysia");
+export const app = new Elysia()
+	.use(appLogger)
+	.use(securityHeaders)
+	.use(errorHandler)
+	.get("/", () => "Hello Elysia");
 
 app.get("/books", () => {
 	return {
@@ -9,7 +17,7 @@ app.get("/books", () => {
 });
 
 if (import.meta.main) {
-	app.listen(3000);
+	app.listen(env.PORT);
 
 	console.log(
 		`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
