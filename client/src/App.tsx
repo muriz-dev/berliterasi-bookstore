1e978ce40121b12688b5e9d9b7e9a4282750c598
stpad <sutanpangeranaryadewa@gmail.com>
2026-08-15 19:31:21 +0700

    rename const at nav-foot.tsx file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,14 @@
-import Navbar from "./components/navbar.jsx";
-import Footer from "./components/footer.jsx";
-import HeadFooter from "./components/headfooter.jsx";
-import Card_sm from "./components/card-sm.jsx";
+import NavFoot from "./layouts/nav-foot";
+
+
+
 
 export default function Home() {
   return (
     <>
       <div className="min-h-screen flex flex-col">
         {/* INI NAVBAR */}
-        <Navbar />
-
-        {/* INI ISINYA */}
-        <main className="grow">
-          <h1>Home</h1>
-          <Card_sm />
-        </main>
-    
-        {/* INI FOOTERNYA */}
-        <HeadFooter />
-        <Footer />
+        <NavFoot />
       </div>
     </>
   );
--- client/src/layouts/nav-foot.tsx
+++ client/src/layouts/nav-foot.tsx
@@ -2,19 +2,22 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import HeadFooter from "../components/headfooter";
 
-export default function Home() {
+const NavFoot = () => {
   return (
     <>
       <div className="min-h-screen flex flex-col">
         {/* INI NAVBAR */}
         <Navbar />
 
+        
 
-    
         {/* INI FOOTERNYA */}
+        <div className="mt-auto">
         <HeadFooter />
         <Footer />
+        </div>
       </div>
     </>
   );
 }
+export default NavFoot;
