7ffdfea4cc1f1d0382ec2e4b9be035b0d7bd56d3
stpad <sutanpangeranaryadewa@gmail.com>
2026-08-25 13:59:57 +0700

    branch for home-page

--- client/src/layouts/nav-foot.tsx
+++ client/src/layouts/nav-foot.tsx
@@ -2,20 +2,22 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import HeadFooter from "../components/headfooter";
 
-const NavFoot = () => {
+const NavFoot = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <div className="min-h-screen flex flex-col">
         {/* INI NAVBAR */}
         <Navbar />
 
-        
+        <main className="flex-grow p-4">
+          {children}
+        </main>
 
         {/* INI FOOTERNYA */}
-        <div className="mt-auto">
-        <HeadFooter />
-        <Footer />
-        </div>
+        
+          <HeadFooter />
+          <Footer />
+
       </div>
     </>
   );
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,11 +1,11 @@
-import NavFoot from "./layouts/nav-foot";
-import Card_sm from "./components/card-sm";
+import NavFoot from "../layouts/nav-foot";
+import Card_sm from "../components/card-sm";
 
 export default function Home() {
   return (
     <>
       <div className="min-h-screen flex flex-col">
-        {/* INI NAVBAR */}
+        
         <NavFoot>
           <Card_sm />
         </NavFoot>
