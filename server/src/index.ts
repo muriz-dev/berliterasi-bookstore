e1eb71972742c4a572f4a75e17a09af3281d934e
Your Name <you@example.com>
2026-08-25 17:51:48 +0700

    feat(server): register openapi and category router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,29 @@
 import { Elysia } from "elysia";
+import { openapi } from '@elysia/openapi'
 import env from "./config/env";
 import { appLogger } from "./utils/logger";
 import { securityHeaders } from "./middlewares/security-header";
 import { errorHandler } from "./middlewares/error-handler";
+import { categoryRoutes } from "./modules/categories/category.route";
 
-export const app = new Elysia()
+const globalMiddlewares = new Elysia()
 	.use(appLogger)
 	.use(securityHeaders)
 	.use(errorHandler)
-	.get("/", () => "Hello Elysia");
+	.use(openapi({
+		documentation: {
+			info: {
+				title: "Elysia API Docs",
+				description: "Elysia API Documentation",
+				version: "1.0.0",
+			},
+		},
+	}));
 
-app.get("/books", () => {
-	return {
-		data: [],
-	};
-});
+export const app = new Elysia()
+	.use(globalMiddlewares)
+	.get("/", () => "Hello Elysia")
+	.use(categoryRoutes);
 
 if (import.meta.main) {
 	app.listen(env.PORT);
